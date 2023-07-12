e8d1a96e8d4f9d52f2306dee1c6f30542a93961d
Jakub Jachowski <jakubjachowski@icloud.com>
2023-07-12 21:40:33 +0200

    minor styling changes, turned on experimental image component, testing how image affects lighthouse score

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind({ applyBaseStyles: false })],
+  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  experimental: {
+    assets: true,
+  },
 });
