82265dc1f72eb63de2d61500eeb28594519cfdb9
Jakub Jachowski <jakubjachowski@icloud.com>
2023-07-12 22:14:12 +0200

    improved colors in Greeting component

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -6,10 +6,12 @@ export default function Greeting({ messages }: Props) {
 
   const [greeting, setGreeting] = useState(messages[0]);
   return (
-    <div className="border-2 border-pink-500 p-2 px-4">
-      <h3>React component! {greeting}!</h3>
+    <div className="border-2 border-blue-600 bg-blue-100 p-2 px-4 dark:border-blue-400 dark:bg-blue-700">
+      <p className="mb-4 text-xl text-blue-600 dark:text-blue-200  sm:mb-0">
+        React component! {greeting}!
+      </p>
       <button
-        className="bg-indigo-400 px-4 py-2"
+        className="border-2 border-blue-500 bg-blue-200 px-4 py-2 text-lg text-blue-500 dark:border-blue-200 dark:bg-blue-500 dark:text-blue-50"
         onClick={() => setGreeting(randomMessage())}
       >
         New Greeting
