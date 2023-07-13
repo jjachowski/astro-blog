282fbfbbc88e66f4f3ea7f560a5db216d66148ac
Jakub Jachowski <jakubjachowski@icloud.com>
2023-07-13 18:52:55 +0200

    framer motion test

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useState } from "react";
 type Props = { messages: string[] };
 export default function Greeting({ messages }: Props) {
@@ -6,16 +7,22 @@ export default function Greeting({ messages }: Props) {
 
   const [greeting, setGreeting] = useState(messages[0]);
   return (
-    <div className="border-2 border-blue-600 bg-blue-100 p-2 px-4 dark:border-blue-400 dark:bg-blue-700">
+    <div className="overflow-hidden border-2 border-blue-600 bg-blue-100 p-2 px-4 dark:border-blue-400 dark:bg-blue-700">
       <p className="mb-4 text-xl text-blue-600 dark:text-blue-200  sm:mb-0">
         React component! {greeting}!
       </p>
+
       <button
-        className="border-2 border-blue-500  bg-blue-200 px-4 py-2 text-lg text-blue-500 dark:border-blue-200 dark:bg-blue-600 dark:text-blue-50"
+        className="border-2 border-blue-500  bg-blue-200 px-4 py-2 text-lg text-blue-500 transition ease-in-out hover:scale-110
+        dark:border-blue-200 dark:bg-blue-600 dark:text-blue-50
+        "
         onClick={() => setGreeting(randomMessage())}
       >
         New Greeting
       </button>
+      <motion.div className="bg-red-300 p-8" whileHover={{ scale: 1.3 }}>
+        hello
+      </motion.div>
     </div>
   );
 }
