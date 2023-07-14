2bf30e37bd54aabefdb6dff228260dd6a24997d6
Jakub Jachowski <jakubjachowski@icloud.com>
2023-07-14 22:48:49 +0200

    removed unnecessary code from react button

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -5,8 +5,8 @@ export default function Greeting({ messages }: Props) {
   const randomMessage = () =>
     messages[Math.floor(Math.random() * messages.length)];
   const [val, setVal] = useState(0);
-
   const [greeting, setGreeting] = useState(messages[0]);
+
   return (
     <div className=" border-2 border-blue-600 bg-blue-100 p-2 px-4 dark:border-blue-400 dark:bg-blue-700">
       <p className="mb-4 text-xl text-blue-600 dark:text-blue-200 sm:mb-2">
@@ -18,11 +18,7 @@ export default function Greeting({ messages }: Props) {
       </p>
 
       <motion.button
-        whileHover={{
-          rotate: [0, 360],
-          borderRadius: [0, "2rem"],
-          scale: 1.4,
-        }}
+        whileHover={{ scale: 1.2 }}
         whileTap={{ scale: 0.7 }}
         transition={{ duration: 1, scale: { duration: 0.2 } }}
         className="m-4 border-2 border-blue-500  bg-blue-200 px-4 py-2 text-lg text-blue-500 
