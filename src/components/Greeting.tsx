576d0db2f12a2a53a3cb125e0f91d71b8b180dc3
Jakub Jachowski <jakubjachowski@icloud.com>
2023-07-15 21:33:38 +0200

    cleaning styles, added typography from tailwind

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -7,7 +7,7 @@ export default function Greeting({ messages }: Props) {
   const [greeting, setGreeting] = useState(messages[0]);
 
   return (
-    <div className=" border-2 border-blue-600 bg-blue-100 p-2 px-4 dark:border-blue-400 dark:bg-blue-700">
+    <div className="border-2 border-blue-600 bg-blue-100 p-2 px-4 dark:border-blue-400 dark:bg-blue-700">
       <p className="mb-4 text-xl text-blue-600 dark:text-blue-200 sm:mb-2">
         React component! {greeting}!
       </p>
