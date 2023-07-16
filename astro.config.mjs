01b7a7935907e7ad42dd9aaeadbc238889e9d577
Jakub Jachowski <jakubjachowski@icloud.com>
2023-07-16 18:53:14 +0200

    about page work in progress, improved typescript

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   experimental: {
     assets: true,
+  },
+  image: {
+    service: sharpImageService(),
   },
 });
