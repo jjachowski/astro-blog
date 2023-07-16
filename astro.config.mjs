c95970b425bccbfd8b295c3f48dd689530907e07
Jakub Jachowski <jakubjachowski@icloud.com>
2023-07-16 19:06:57 +0200

    styling to about page, added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig, sharpImageService } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: "https://www.jachowski.dev",
+  integrations: [react(), tailwind(), sitemap()],
   experimental: {
     assets: true,
   },
