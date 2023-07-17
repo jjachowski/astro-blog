f625bc1ffe6392c51c0254c32cab9c6e07c3f460
Jakub Jachowski <jakubjachowski@icloud.com>
2023-07-17 15:32:11 +0200

    refactored tags, added new style and tags count

--- src/utils/markdownUtils.ts
+++ src/utils/markdownUtils.ts
@@ -1,7 +1,12 @@
 import type { MarkdownInstance } from "astro";
 
-export function getUniqueTags(
-	posts: MarkdownInstance<Record<string, any>>[],
-): string[] {
-	return Array.from(new Set(posts.flatMap((x) => x.frontmatter.tags)));
+export function getUniqueTags(posts: MarkdownInstance<Record<string, any>>[]) {
+  const tagsFlatMap = posts.flatMap((x) => x.frontmatter.tags);
+
+  return Array.from(new Set(posts.flatMap((x) => x.frontmatter.tags))).map(
+    (x) => ({
+      text: x as string,
+      count: tagsFlatMap.filter((y) => y === x).length,
+    })
+  );
 }
