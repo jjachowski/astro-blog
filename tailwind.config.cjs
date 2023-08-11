b0f9905dbb78380bc7f8f241fb8bfe1c4fac776a
Jakub Jachowski <jakubjachowski@icloud.com>
2023-08-11 22:40:38 +0200

    started adding new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,21 @@ module.exports = {
       screens: {
         xs: "380px",
       },
+      colors: {
+        green: {
+          50: "#f6f7ee",
+          100: "#eaedda",
+          200: "#d6dcba",
+          300: "#bbc591",
+          400: "#a1ae6d",
+          500: "#84934f",
+          600: "#606c38",
+          700: "#4f5a31",
+          800: "#41492b",
+          900: "#383f28",
+          950: "#1d2112",
+        },
+      },
     },
     fontFamily: {
       jetbrains: ["JetBrains Mono Variable", "monospace"],
